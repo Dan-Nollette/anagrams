965f3288bdfda03c1f54418764f2a9201eaee272
Dan Nollette <nollette.dan@gmail.com>
2017-09-16 22:57:32 -0700

    Add code to make spec two pass (second part of first assignment instruction.)

--- lib/anagram_checker.rb
+++ lib/anagram_checker.rb
@@ -1,5 +1,13 @@
 class Anagram_checker
   def self.anagrams_check(parameters)
-    "Sorry, those aren't anagrams"
+    word1 = parameters.fetch(:word1)
+    word2 = parameters.fetch(:word2)
+    letters1 = word1.split("").sort
+    letters2 = word2.split("").sort
+    if (letters1 == letters2)
+      return "Yes, those are anagrams"
+    else
+      return "Sorry, those aren't anagrams"
+    end
   end
 end
--- spec/anagram_checker_spec.rb
+++ spec/anagram_checker_spec.rb
@@ -10,29 +10,29 @@ describe('anagram_checker') do
       expect(Anagram_checker.anagrams_check({word1: "ruby", word2: "bury"})).to(eq("Yes, those are anagrams"))
     end
   end
-  describe('.anagrams_check') do
-    it('accounts for the possibility that words might have different cases but should still be anagrams.') do
-      expect(Anagram_checker.anagrams_check({word1: "Ruby", word2: "Bury"})).to(eq("Yes, those are anagrams"))
-    end
-  end
-  describe('.anagrams_check') do
-    it('checks if two anagrams are also form a palindrome, returning an explanatory string.') do
-      expect(Anagram_checker.anagrams_check({word1: "Eat", word2: "Tea"})).to(eq("Yes, those are anagrams, and also form a palindrome"))
-    end
-  end
-  describe('.anagrams_check') do
-    it('checks if inputs are words (contain vowels).') do
-        expect(Anagram_checker.anagrams_check({word1: "WV", word2: "VW"})).to(eq("You need to input actual words!"))
-    end
-  end
-  describe('.anagrams_check') do
-    it('checks if non anagrams are antigrams.') do
-      expect(Anagram_checker.anagrams_check({word1: "ruby", word2: "damp"})).to(eq("These words have no letter matches and are antigrams."))
-    end
-  end
-  describe('.anagrams_check') do
-    it('ignores spacing and punctuation to only consider letters in a multiword phrase.') do
-      expect(Anagram_checker.anagrams_check({word1: "Dan Nollette", word2: "Nellda T Note"})).to(eq("Yes, those are anagrams"))
-    end
-  end
+  # describe('.anagrams_check') do
+  #   it('accounts for the possibility that words might have different cases but should still be anagrams.') do
+  #     expect(Anagram_checker.anagrams_check({word1: "Ruby", word2: "Bury"})).to(eq("Yes, those are anagrams"))
+  #   end
+  # end
+  # describe('.anagrams_check') do
+  #   it('checks if two anagrams are also form a palindrome, returning an explanatory string.') do
+  #     expect(Anagram_checker.anagrams_check({word1: "Eat", word2: "Tea"})).to(eq("Yes, those are anagrams, and also form a palindrome"))
+  #   end
+  # end
+  # describe('.anagrams_check') do
+  #   it('checks if inputs are words (contain vowels).') do
+  #       expect(Anagram_checker.anagrams_check({word1: "WV", word2: "VW"})).to(eq("You need to input actual words!"))
+  #   end
+  # end
+  # describe('.anagrams_check') do
+  #   it('checks if non anagrams are antigrams.') do
+  #     expect(Anagram_checker.anagrams_check({word1: "ruby", word2: "damp"})).to(eq("These words have no letter matches and are antigrams."))
+  #   end
+  # end
+  # describe('.anagrams_check') do
+  #   it('ignores spacing and punctuation to only consider letters in a multiword phrase.') do
+  #     expect(Anagram_checker.anagrams_check({word1: "Dan Nollette", word2: "Nellda T Note"})).to(eq("Yes, those are anagrams"))
+  #   end
+  # end
 end
